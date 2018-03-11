51d44567cb30bece762aeed1a81e1024834654ea
GardelCoder <gardelitoo@hotmail.com.ar>
2018-03-11 19:49:51 -0300

    [First commit]

---
+++ src/Entities/Person.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author ratzinger
+ */
+public class Person {
+    
+    private int dni;
+    
+    private String name;
+
+    public Person(int dni, String name) {
+        this.dni = dni;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + this.dni;
+        hash = 73 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (this.dni != other.dni) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "dni=" + dni + ", name=" + name + '}';
+    }
+    
+    
+    
+}
